679a91ee7d79a752c44343b6b9e0434a42381c1b
Rong Ling <rongling@Rongs-MacBook-Pro.local>
2025-11-04 21:51:49 -0800

    udpate main.ts

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,12 @@ import * as TradingState from './models/tradingState';
 import * as TradingPlans from './models/tradingPlans/tradingPlans';
 import * as TvTools from './tools/tradingview';
 import * as TraderFocus from './controllers/traderFocus';
+import * as StreamingHandler from './controllers/streamingHandler';
+import * as KeyboardHandler from './controllers/keyboardHandler';
+import * as AlpacaStreaming from './api/alpaca/streaming';
+import * as ScwabStreaming from './api/schwab/streaming';
+import * as DB from './data/db';
+import './tosClient';
 
 declare let window: Models.MyWindow;
 
@@ -168,3 +174,55 @@ if (testPopButton) {
 
 Firestore.addToLogView('version 1.34', 'Info');
 
+let now = new Date();
+
+window.TradingApp.TOS.initialize().then(async () => {
+    // tos initialized with new access token
+    // tos access token expires in 30 minutes, so refresh before that
+    // tradestation token expires in 20 minutes
+    setInterval(Broker.refreshAccessToken, 1150 * 1000);
+    // create watchlist and setup chart
+    Chart.setup();
+    let timeframe = 1;
+
+    Models.setTimeframe(timeframe);
+
+    // open web socket
+    AlpacaStreaming.createWebSocketForMarketData();
+    AlpacaStreaming.createWebSocket();
+    ScwabStreaming.createWebSocket();
+    let previousDate = await MarketData.getPreviousTradingDate();
+
+
+    // get price history
+    let watchlist = Models.getWatchlist();
+    for (let i = 0; i < watchlist.length; i++) {
+        let symbol = watchlist[i].symbol;
+        let marketCap = Models.getMarketCapInMillions(symbol);
+        if (!marketCap) {
+            alert(`no market cap for ${symbol}`);
+        } else if (marketCap < 500) {
+            alert(`${symbol} market cap too low, only $ ${marketCap} M`);
+            return;
+        }
+        MarketData.getPriceHistory(symbol, Helper.isFutures(symbol), timeframe).then((candles) => {
+            // populate current chart
+            DB.initialize(symbol, candles);
+            Chart.updateAccountUIStatusForSymbol(symbol);
+            if (now > Helper.getMarketOpenTime()) {
+                AutoTrader.onMarketOpen(symbol);
+            }
+            MarketData.setPreviousDayPremarketVolume(symbol, previousDate);
+        });
+    }
+    UI.setupAutoSync();
+    AutoTrader.scheduleEvents();
+    // MarketData.testTradeStationStreamBar();
+});
+
+let htmlBody = document.getElementsByTagName("body")[0];
+htmlBody.addEventListener("keydown", async function (keyboardEvent) {
+    let code = keyboardEvent.code;
+    let shiftKey = keyboardEvent.shiftKey;
+    KeyboardHandler.handleKeyPressed(code, shiftKey);
+});
