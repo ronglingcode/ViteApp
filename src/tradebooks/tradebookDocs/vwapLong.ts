2beae14b6207518398b8cdb90994ef8d7369dbd2
Rong Ling <wumnlr@gmail.com>
2025-12-03 22:23:34 -0800

    update vwap trade management

--- src/tradebooks/tradebookDocs/vwapLong.ts
+++ src/tradebooks/tradebookDocs/vwapLong.ts
@@ -7,10 +7,15 @@ the stop-out criteria can be adjusted to allow for more flexibility.
 2. Instead, use a more lenient rule: only stop out if a full one-minute candle closes below VWAP.
 3. This approach helps avoid premature exits and allows the remaining partial position more room to fluctuate, ensuring the trade has a fair chance to continue in your favor.
 
+[Consolidation between vwap and premarket high]
 If the price is moving between vwap and premarket high, try not do anything. 
 The price action can be choppy when it's above vwap but below premarket high.
 Only take partial when it's near premarket high or add back partials when it's near vwap.
 It will be better to add when it breakout premarket high. 
 The long is the consolidation between vwap and premarket high, the better long setup is the breakout of premarket high.
+
+[Retracement to vwap]
+When price is moving back down, don't get scared and pay attention to whether the price can get back below vwwap or close a candle below vwap.
+As long as it doesn't do that, any move back down to vwap is a good opportunity to add back partials.
 
 `.trim();
--- src/tradebooks/tradebookDocs/vwapShort.ts
+++ src/tradebooks/tradebookDocs/vwapShort.ts
@@ -7,9 +7,14 @@ the stop-out criteria can be adjusted to allow for more flexibility.
 2. Instead, use a more lenient rule: only stop out if a full one-minute candle closes above VWAP.
 3. This approach helps avoid premature exits and allows the remaining partial position more room to fluctuate, ensuring the trade has a fair chance to continue in your favor.
 
+[Consolidation between vwap and premarket low]
 If the price is moving between vwap and premarket low, try not do anything. 
 The price action can be choppy when it's below vwap but above premarket low.
 Only take partial when it's near premarket low or add back partials when it's near vwap.
 It will be better to add when it breakdown premarket low. 
 The long is the consolidation between vwap and premarket low, the better short setup is the breakdown of premarket low.
+
+[Retracement to vwap]
+When price is moving back up, don't get scared and pay attention to whether the price can get back above vwwap or close a candle above vwap.
+As long as it doesn't do that, any move back up to vwap is a good opportunity to add back partials.
 `.trim();
