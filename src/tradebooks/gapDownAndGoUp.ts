befcf86c72ff2933220fdbdba06b816a37da841e
Rong Ling <wumnlr@gmail.com>
2026-03-13 06:01:01 -0700

    check tradebook

--- src/tradebooks/gapDownAndGoUp.ts
+++ src/tradebooks/gapDownAndGoUp.ts
@@ -16,9 +16,7 @@ export class GapDownAndGoUp extends Tradebook {
      * Otherwise logs error and returns false.
      */
     public static hasAtLeastOneReasonSet(plan: TradingPlansModels.GapDownAndGoUpPlan, symbol: string): boolean {
-        const hasOne =
-            !!plan.nearAboveSupport ||
-            !!plan.nearAboveKeyEventLevel;
+        const hasOne = (plan.nearAboveSupport) || plan.nearAboveKeyEventLevel;
         if (!hasOne) {
             Firestore.logError(`${symbol} missing one reason set for gap down and go up plan`);
             return false;
