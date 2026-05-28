43e8b9da951e7207608abf65b81e15b064790d71
Rong Ling <wumnlr@gmail.com>
2026-05-28 09:10:11 -0700

    smaller chart

--- src/ui/chartSettings.ts
+++ src/ui/chartSettings.ts
@@ -88,8 +88,10 @@ export const getChartSettings = (tabIndex: number, totalCount: number) => {
     let width = focusChartSize.width;
     let height = focusChartSize.height;
     if (totalCount === 1) {
-        width = bigChartSize.width;
-        height = bigChartSize.height;
+        //width = bigChartSize.width;
+        //height = bigChartSize.height;
+        width = halfChartSize.width;
+        height = halfChartSize.height;
     } else if (totalCount == 2) {
         width = halfChartSize.width;
         height = halfChartSize.height;
