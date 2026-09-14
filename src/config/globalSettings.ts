42b84d86da1d77df5afdeecb630c100b249c85e1
ronglingcode <69086027+ronglingcode@users.noreply.github.com>
2026-09-14 06:23:01 -0700

    add whitelist

--- src/config/globalSettings.ts
+++ src/config/globalSettings.ts
@@ -2,7 +2,7 @@ export const localhostWithPort: string = "http://localhost:3000";
 export const batchCount: number = 10;
 export const impliedMarketCapThresholdInBillions: number = 0.9;
 export const premarketVolumeThresholdInMillions: number = 0.9;
-export const premarketVolumeThresholdWhitelist: string[] = [];
+export const premarketVolumeThresholdWhitelist: string[] = ['CRWD'];
 // Block trading when the watchlist contains more than this many stocks.
 export const maxTradableStocksCount: number = 1; // do not change, trade management requires full attention
 // true: offload time & sales socket receipt + parsing to a Web Worker (main app).
